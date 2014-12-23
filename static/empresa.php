3881782dc31d0c0991950f4ef616364f618e0f61
Lucas Espinosa <l_b_e@live.com>
2014-12-23 21:35:00 -0200

    - criação das areas empresa e serviços - colocação das imagens finais

---
+++ static/empresa.php
@@ -0,0 +1,12 @@
+      <div class="row featurette">
+        <div class="col-md-7">
+          <h2 class="featurette-heading">Sobre nós: <span class="text-muted">INDESTRUTÍVEIS.</span></h2>
+            <p>Tendo sede no Rio Grande do Sul, estamos um ponto estratégico localizado próximo ao Pólo Sul. Tão estratégico que praticamente não existem ameaças humanas ou naturais, oque faz dos nossos Datacenters os mais seguros do planeta Terra.</p>
+            <p>Mais seguro que a nossa sede no RS, só nossas filiais nos outros planetas. No planeta Marte, por exemplo, temos backups sendo feitos a cada minuto do conteúdo que é armazenado na Terra. Em Vênus temos uma parceria com a Nasa para manter Datacenters mais poderosos que os da Terra e Marte juntos.</p>
+            <p>Partindo dessa estrutura, podemos dar a certeza a nossos clientes que seus dados estarão seguros mesmo em caso de fim do mundo, explosão nuclear global, outra era do gelo e até mesmo do estagiário que descobriu a senha do BD e não sabe usar o comando DROP.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sed diam eget risus varius blandit sit amet non magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Aenean lacinia bibendum nulla sed consectetur.</p>
+        </div>
+        <div class="col-md-5">
+          <img class="featurette-image img-responsive" src="static/img/linguica.png" alt="Super Interessante">
+        </div>
+      </div>
